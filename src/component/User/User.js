0bddd2b715bc3285f86be9e5012440c88b78557a
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-02-09 09:05:36 +0530

    User UI

--- src/component/User/User.js
+++ src/component/User/User.js
@@ -1,9 +1,19 @@
-import React, { Fragment,Component } from 'react'
+import React, { Fragment,Component } from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 class User extends Component {
     componentDidMount(){
         this.props.singleUser(this.props.match.params.login);
     }
+
+    static  propTypes = {
+        loading:PropTypes.bool.isRequired,
+        singleUser:PropTypes.func.isRequired,
+        users:PropTypes.object.isRequired,
+    }
+    
 
     render() {
         const {
@@ -22,12 +32,46 @@ class User extends Component {
         } = this.props.users;
 
         const {loading} = this.props;
+
+        if(loading){
+            return <Spinner/>
+        }
+       
         return (
             <Fragment>
-               {location}
+               <Link to='/' className="btn btn-dark ">Back to Search</Link>
+               
+               Hireable:{' '}
+               {hireable ? (<i className="fa fa-check text-success" />) :(<i className="fa fa-times-circle text-danger"/>) }
+
+               <div className="card grid-2">
+                    <div className="all-center">
+                        <img
+                        src={avatar_url}
+                        className="round-img"
+                        alt=""
+                        style={{width:'150px'}}
+                        />
+                        <h1>{name}</h1>
+                        <p>Location: {location}</p>
+                        <p>Followers: {followers}</p>
+                        <p>Following: {following}</p>
+                    </div>
+                    <div>
+                        {bio &&
+                        <Fragment>
+                            <h2>Bio 😎</h2>
+                            <p>{bio}</p>
+                        </Fragment>
+                        }
+                        <a href={html_url} className="btn btn-dark my-1">Visit Github Profile</a>
+                    </div>
+               </div>
             </Fragment>
         )
     }
 }
+
+
 
 export default User;
